a512a10ee6e86598480923a6e5e3c9a9268bea50
alefew <alefewyimer4@gmail.com>
2023-12-21 21:26:40 +0300

    refacor models

---
+++ models/request.go
@@ -0,0 +1,15 @@
+package models
+
+import "github.com/google/uuid"
+
+// CreateVoteRequest represents the request model for creating a vote.
+type CreateVoteRequest struct {
+	PollID   uuid.UUID `json:"poll_id" binding:"required"`
+	Key      string    `json:"key" binding:"required"`
+	ClientId string    `json:"client_id" binding:"required"`
+}
+
+// CreatePresentationRequest represents the request model for creating a presentation.
+type CreatePresentionRequest struct {
+	Polls []Poll `json:"polls"`
+}
